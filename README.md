f0b8c9cdfe8331c28f8ec371bb64ddd7d4630601
Heberth <heberthardila@gmail.com>
2017-06-06 11:35:35 -0500

    Se crea script para intalacion rapida
    Se

--- README.md
+++ README.md
@@ -11,24 +11,23 @@ Algunos scripts que encontrará aca:
 * Plugins de nagios varios
 
 ## Instalación Nagios Core 4.3.1
-Validado en : Debian 7+ 
+Validado en : Debian 7+
 ```
-curl -k https://raw.githubusercontent.com/dorancemc/nagios_core/master/install_nagios.sh | sh -x
+curl -k https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/install_nagios.sh | sh -x
 ```
 
 ## Instalación cliente NRPE 3.1.0
-Validado en : Debian 6+, Ubuntu 16+, Centos 6+, OpenSuSE 42
+Validado en : Debian 6+, Ubuntu 16+, Centos 6+, OpenSuSE 12
 ```
-curl -k https://raw.githubusercontent.com/dorancemc/nagios_core/master/install_nrpe.sh | bash -x
+curl -k https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/install_nrpe.sh | bash -x
 ```
 
 ## Instalación Nagios Plugins 2.1.3
-Validado en : Debian 6+, Ubuntu 16+, Centos 6+
+Validado en : Debian 6+, Ubuntu 16+, Centos 6+, OpenSuse 12
 ```
-curl -k https://raw.githubusercontent.com/dorancemc/nagios_core/master/install_nagiosplugins.sh | sh -x
+curl -k https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/install_nagiosplugins.sh | sh -x
 ```
 Si tiene problemas para instalar los complementos de Perl, ejecute lo siguiente:
 ````
-wget http://search.cpan.org/CPAN/authors/id/N/NA/NAGIOS/Nagios-Monitoring-Plugin-0.51.tar.gz && tar -zxvf Nagios-Monitoring-Plugin-0.51.tar.gz && cd Nagios-Monitoring-Plugin-0.51 && perl Makefile.PL ; make ; make install 
+wget http://search.cpan.org/CPAN/authors/id/N/NA/NAGIOS/Nagios-Monitoring-Plugin-0.51.tar.gz && tar -zxvf Nagios-Monitoring-Plugin-0.51.tar.gz && cd Nagios-Monitoring-Plugin-0.51 && perl Makefile.PL ; make ; make install
 ```
-
---
+++ install_configure_cliente_plugins.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Se intalara el cliente NRPE y se Configurara"
+
+echo "Instalando NRPE..."
+sleep 1
+curl -k https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/install_nrpe.sh | bash -x
+
+echo "instalando dependencias"
+sleep 1
+zypper --non-interactive install sysstat
+
+echo "Instalando los plugins de nagios"
+sleep 1
+curl -k https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/install_nagiosplugins.sh | sh -x
+
+
+echo "Backup del archivo "
+sleep 1
+cp /opt/nagios/etc/nrpe.cfg /opt/nagios/etc/nrpe.cfgbackup
+
+echo "Eliminando archivo anterior nrpe.cfg..."
+sleep 1
+rm /opt/nagios/etc/nrpe.cfg
+
+echo "Copiando archivo de configuracion y cambiando dueño a nagios"
+sleep 1
+cd /opt/nagios/etc/ && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/conf-base/nrpe.cfg && chown nagios:nagios nrpe.cfg
+
+echo "Descargando archivo de configuracion para los check base"
+sleep 1
+cd /opt/nagios/etc/nrpe/ && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/conf-base/check_base.cfg && chmod +x check_base.cfg
+
+echo "Pasando los check base al directorio other"
+
+if [ -d "/opt/nagios/libexec" ]; then
+    cd /opt/nagios/libexec/ && mkdir other && cd other && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_await.sh && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_cpu.sh && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_iops.sh && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_mem.sh && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_netint.pl && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_users_ip.pl
+else
+    cd /opt/nagios/lib/ && mkdir other && cd other && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_await.sh && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_cpu.sh && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_iops.sh && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_mem.sh && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_netint.pl && wget https://raw.githubusercontent.com/UavLabsColombia/nagios_core/master/check-base/check_users_ip.pl
+
+echo "Es necesario agregar la ip del servidor Nagios Core para que acepte las peticiones sobre /opt/nagios/etc/nrpe.cfg en la variabla allowed_hosts... "
+sleep 1
